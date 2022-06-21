4f0daf038f7a84bf5e3ea92c70b8897bace359d9
beyzabostanci <beyza_bostanc@hotmail.com>
2022-06-22 00:14:20 +0300

    completed

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleCat(e){
+    setCategory(e.target.value)
+  }
+
+  function handleNam(e){
+    setName(e.target.value)
+  }
+
+  function handleSub(e){
+    e.preventDefault();
+    onItemFormSubmit({
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: name,
+      category: category,
+    });
+  }
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSub}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNam} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={handleCat}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
